a647cde9b28b1f13a75cde9bbb6d4b97a5abe2cd
kriswep <kriswep@wetainment.com>
2021-04-24 15:30:47 +0200

    add layout

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,21 +1,5 @@
-import { Link, routes } from '@redwoodjs/router'
-
 const HomePage = () => {
-  return (
-    <>
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>Home</main>
-    </>
-  )
+  return <>Home</>
 }
 
 export default HomePage
