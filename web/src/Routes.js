5347256e005b78ab052d2ecea36f6866276f55af
kriswep <kriswep@wetainment.com>
2021-04-27 22:18:56 +0200

    add authentication

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,16 +7,18 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Router, Route, Set, Private } from '@redwoodjs/router'
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
 const Routes = () => {
   return (
     <Router>
+      {/* <Private unauthenticated="home"> */}
       <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
       <Route path="/admin/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
       <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
       <Route path="/admin/posts" page={PostsPage} name="posts" />
+      {/* </Private> */}
       <Set wrap={BlogLayout}>
         <Route path="/contact" page={ContactPage} name="contact" />
         <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
